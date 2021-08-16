1b8bdd5433ec7e1395045391330b1ad08da87ce6
rupeshgithub-viit <63735887+rupeshgithub-viit@users.noreply.github.com>
2021-08-16 18:41:33 +0530

    Add files via upload

--- contactlist/routes/route.js
+++ contactlist/routes/route.js
@@ -1,1 +1,48 @@
+const express = require('express');
+const contact = require('../models/contacts');
+const router = express.Router();
+const Contact = require('../models/contacts');
 
+router.get('/contacts',(req,res,next)=>{
+    //res.send('Retriving the contact list');
+
+    Contact.find(function(err,contact){
+        res.json(contact);
+    })
+});
+
+
+router.post('/contact',(req,res,next)=>{
+    //logic
+    let newContact = new Contact({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone: req.body.phone
+    });
+
+    newContact.save((err, contact)=>{
+        if(err){
+            res.json({msg: 'Failed toadd contact'});
+        }
+        else{
+            res.json({msg: 'Contact added successfully'});
+        }
+    });
+});
+
+//delete
+router.delete('/contact/:id',(req,res,next)=>{
+    //logic
+    Contact.remove({_id: req.params.id}, function(err,result){
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.json(result);
+        }
+    });
+
+
+});
+
+module.exports = router;
